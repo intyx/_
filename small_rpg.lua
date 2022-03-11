1e850e81721c189bf76355d5cd65350eb27f5772
intyx <101004838+intyx@users.noreply.github.com>
2022-03-11 20:00:42 +0300

    Create small_rpg.lua

---
+++ small_rpg.lua
@@ -0,0 +1,41 @@
+local plr = game:GetService("Players").LocalPlayer; plr.Character.Humanoid:UnequipTools()
+local Tools = plr.Backpack:GetChildren()
+
+Tools[1].Grip = CFrame.new(0.7, -0.5, -0.2) * CFrame.Angles(50, math.rad(-180), 3.13)
+Tools[1].Parent = plr.Character
+
+Tools[2].Grip = CFrame.new(0.7, -1.2, -0.2) * CFrame.Angles(1.6, math.rad(-98),1.6)
+Tools[2].Parent = plr.Character
+
+Tools[3].Grip = CFrame.new(2.5, -1.2, -0.2) * CFrame.Angles(1.6, math.rad(-98),1.6)
+Tools[3].Parent = plr.Character
+
+Tools[4].Grip = CFrame.new(4.3, -1.2, -0.2) * CFrame.Angles(1.6, math.rad(-98),1.6)
+Tools[4].Parent = plr.Character
+
+Tools[5].Grip = CFrame.new(-6.5, -1.6, -0.2) * CFrame.Angles(1.6, math.rad(-250),1.6)
+Tools[5].Parent = plr.Character
+
+Tools[6].Grip = CFrame.new(6, -2.55, -0.2) * CFrame.Angles(1.6, math.rad(-115),1.6)
+Tools[6].Parent = plr.Character
+
+Tools[7].Grip = CFrame.new(1.55, 7.2, 0.1) * CFrame.Angles(1.6, math.rad(-10),1.6)
+Tools[7].Parent = plr.Character
+
+Tools[8].Grip = CFrame.new(-1.1, -1.2, -0.23) * CFrame.Angles(1.6, math.rad(-98),1.57)
+Tools[8].Parent = plr.Character
+
+Tools[9].Grip = CFrame.new(-2.8, -1.17, -0.17) * CFrame.Angles(1.6, math.rad(-99),1.6)
+Tools[9].Parent = plr.Character
+
+Tools[10].Grip = CFrame.new(0, 2.7, -0.2) * CFrame.Angles(1.575, math.rad(-175),1.575)
+Tools[10].Parent = plr.Character
+
+Tools[11].Grip = CFrame.new(-4.6, -1.1, -0.17) * CFrame.Angles(1.6, math.rad(-100),1.6)
+Tools[11].Parent = plr.Character
+
+Tools[12].Grip = CFrame.new(-6.3, -1.1, -0.17) * CFrame.Angles(1.6, math.rad(-100),1.6)
+Tools[12].Parent = plr.Character
+
+Tools[13].Grip = CFrame.new(-1, 6, -0.45) * CFrame.Angles(1.6, math.rad(-205),1.4)
+Tools[13].Parent = plr.Character
