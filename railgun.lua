66464b55dc2156a0f6edae21ea2b44966faad4a3
intyx <101004838+intyx@users.noreply.github.com>
2022-03-11 20:00:09 +0300

    Create railgun.lua

---
+++ railgun.lua
@@ -0,0 +1,71 @@
+local plr = game:GetService("Players").LocalPlayer; plr.Character.Humanoid:UnequipTools()
+local Tools = plr.Backpack:GetChildren()
+
+Tools[1].Grip = CFrame.new(1.1, -1.2, 0) * CFrame.Angles(-1.58, math.rad(-108), -1.58)
+Tools[1].Parent = plr.Character
+
+Tools[2].Grip = CFrame.new(2.7, -1.2, 0.02) * CFrame.Angles(-1.58, math.rad(-108), -1.58)
+Tools[2].Parent = plr.Character
+
+Tools[3].Grip = CFrame.new(4.5, -2.34, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[3].Parent = plr.Character
+
+Tools[4].Grip = CFrame.new(4, 5.1, -0.07) * CFrame.Angles(-1.58, math.rad(-180), -1.58)
+Tools[4].Parent = plr.Character
+
+Tools[5].Grip = CFrame.new(-5.6, 0.9, -0.07) * CFrame.Angles(-1.58, math.rad(-305), -1.58)
+Tools[5].Parent = plr.Character
+
+Tools[6].Grip = CFrame.new(-1.29, 3.5, -0.07) * CFrame.Angles(-1.58, math.rad(-262), -1.58)
+Tools[6].Parent = plr.Character
+
+Tools[7].Grip = CFrame.new(4, 1, -0.07) * CFrame.Angles(-1.58, math.rad(-193), -1.58)
+Tools[7].Parent = plr.Character
+
+Tools[8].Grip = CFrame.new(-2.8, -0.8, 0) * CFrame.Angles(-1.58, math.rad(-25), -1.58)
+Tools[8].Parent = plr.Character
+
+Tools[9].Grip = CFrame.new(0.8, 4.8, -0.07) * CFrame.Angles(-1.58, math.rad(-270), -1.58)
+Tools[9].Parent = plr.Character
+
+Tools[10].Grip = CFrame.new(-2.3, -3.8, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[10].Parent = plr.Character
+
+Tools[11].Grip = CFrame.new(-3, -3.8, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[11].Parent = plr.Character
+
+Tools[12].Grip = CFrame.new(3, 4.8, -0.075) * CFrame.Angles(-1.58, math.rad(-270), -1.58)
+Tools[12].Parent = plr.Character
+
+Tools[13].Grip = CFrame.new(4.3, 5.6, -0.075) * CFrame.Angles(-1.58, math.rad(-270), -1.58)
+Tools[13].Parent = plr.Character
+
+Tools[14].Grip = CFrame.new(-4.3, -3.2, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[14].Parent = plr.Character
+
+Tools[15].Grip = CFrame.new(6.6, 5.6, -0.075) * CFrame.Angles(-1.58, math.rad(-270), -1.58)
+Tools[15].Parent = plr.Character
+
+Tools[16].Grip = CFrame.new(-6.6, -3.2, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[16].Parent = plr.Character
+
+Tools[17].Grip = CFrame.new(8.8, 5.6, -0.075) * CFrame.Angles(-1.58, math.rad(-270), -1.58)
+Tools[17].Parent = plr.Character
+
+Tools[18].Grip = CFrame.new(-8.8, -3.2, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[18].Parent = plr.Character
+
+Tools[19].Grip = CFrame.new(11, 5.6, -0.075) * CFrame.Angles(-1.58, math.rad(-270), -1.58)
+Tools[19].Parent = plr.Character
+
+Tools[20].Grip = CFrame.new(-11, -3.2, 0) * CFrame.Angles(-1.58, math.rad(-90), -1.58)
+Tools[20].Parent = plr.Character
+
+Tools[21].Grip = CFrame.new(3.7, 4.2, -0.075) * CFrame.Angles(-1.58, math.rad(-250), -1.58)
+Tools[21].Parent = plr.Character
+
+Tools[22].Grip = CFrame.new(-0.8, -4.1, 0) * CFrame.Angles(-1.58, math.rad(250), -1.58)
+Tools[22].Parent = plr.Character
+
+Tools[23].Grip = CFrame.new(-0.4, 0.7, -0) * CFrame.Angles(1.58, math.rad(-164), 1.58)
+Tools[23].Parent = plr.Character
